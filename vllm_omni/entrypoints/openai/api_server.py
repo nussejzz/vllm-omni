cfd1d1db3f9e0695f2c9fa06ad753f07b1cd6b47
wuhang <whlbx@hotmail.com>
2026-03-12 20:54:53 +0800

    resolve config (#7)
    * resolve full config
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    * resolve config
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    * resolve config
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    * resolve conf
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    * resolve conf
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    * precommit
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    * precommit
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    ---------
    
    Signed-off-by: wuhang <wuhang6@huawei.com>
    
    Signed-off-by: wuhang <whlbx@hotmail.com>

--- vllm_omni/entrypoints/openai/api_server.py
+++ vllm_omni/entrypoints/openai/api_server.py
@@ -83,6 +83,7 @@ from vllm.tasks import POOLING_TASKS
 from vllm.tool_parsers import ToolParserManager
 from vllm.utils.system_utils import decorate_logs
 
+from vllm_omni.engine.arg_utils import OmniEngineArgs
 from vllm_omni.entrypoints.async_omni import AsyncOmni
 from vllm_omni.entrypoints.openai.errors import InvalidInputReferenceError
 from vllm_omni.entrypoints.openai.image_api_utils import (
@@ -390,11 +391,8 @@ async def build_async_omni_from_stage_config(
     async_omni: EngineClient | None = None
 
     try:
-        # Convert args Namespace to kwargs dict for AsyncOmni to use
-        kwargs = vars(args).copy()
-        # Remove model as it will be passed separately
-        kwargs.pop("model", None)
-        async_omni = AsyncOmni(model=args.model, **kwargs)
+        engine_args = OmniEngineArgs.from_cli_args(args)
+        async_omni = AsyncOmni(model=engine_args.model, engine_args=engine_args)
 
         # # Don't keep the dummy data in memory
         # await async_llm.reset_mm_cache()
