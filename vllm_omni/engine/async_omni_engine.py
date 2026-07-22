b5c8a7ecfb72cf94fafa6826197a4330380d7483
Ding Zuhao <e1583181@u.nus.edu>
2026-07-22 16:49:11 +0800

    [Core] Add BAGEL KV-gather context parallelism
    Signed-off-by: Ding Zuhao <e1583181@u.nus.edu>

--- vllm_omni/engine/async_omni_engine.py
+++ vllm_omni/engine/async_omni_engine.py
@@ -968,6 +968,11 @@ class AsyncOmniEngine:
         if parallel_config is None:
             ulysses_degree = normalized_kwargs.get("ulysses_degree") or 1
             ring_degree = normalized_kwargs.get("ring_degree") or 1
+            context_parallel_degree = normalized_kwargs.get("context_parallel_degree")
+            if context_parallel_degree is None:
+                from vllm_omni.diffusion import envs
+
+                context_parallel_degree = envs.VLLM_OMNI_CP_DEGREE
             ulysses_mode = normalized_kwargs.get("ulysses_mode") or "strict"
             sequence_parallel_size = normalized_kwargs.get("sequence_parallel_size")
             pipeline_parallel_size = normalized_kwargs.get("pipeline_parallel_size") or 1
@@ -982,7 +987,7 @@ class AsyncOmniEngine:
             hsdp_shard_size = normalized_kwargs.get("hsdp_shard_size", -1)
             hsdp_replicate_size = normalized_kwargs.get("hsdp_replicate_size", 1)
             if sequence_parallel_size is None:
-                sequence_parallel_size = ulysses_degree * ring_degree
+                sequence_parallel_size = ulysses_degree * ring_degree * context_parallel_degree
 
             parallel_config = DiffusionParallelConfig(
                 pipeline_parallel_size=pipeline_parallel_size,
@@ -992,6 +997,7 @@ class AsyncOmniEngine:
                 sequence_parallel_size=sequence_parallel_size,
                 ulysses_degree=ulysses_degree,
                 ring_degree=ring_degree,
+                context_parallel_degree=context_parallel_degree,
                 ulysses_mode=ulysses_mode,
                 cfg_parallel_size=cfg_parallel_size,
                 vae_patch_parallel_size=vae_patch_parallel_size,
